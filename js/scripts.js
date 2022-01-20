3c582f7f94139ce73c40b44d6650c916076f95c6
Zninor <97253866+Zninor@users.noreply.github.com>
2022-01-20 09:43:38 -0800

    2.3 hamburger update

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
 console.log("Welcome to my portfolio!");
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
+
+// code to make the site scroll to a specific place right away
+//window.scroll({
+  //top: 200,
+//  left: 0,
+//  behavior: 'smooth'});
