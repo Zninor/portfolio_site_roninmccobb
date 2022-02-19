892b2f72a9ce14da44127ff81db2dffd51edd598
Zninor <97253866+Zninor@users.noreply.github.com>
2022-02-18 17:45:36 -0800

    animation deleted

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-console.log("Welcome to my portfolio!");
 
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
@@ -7,10 +6,12 @@ function menuToggle() {
   } else {
     x.className = 'navtoggle';
   }
+
+
 }
 
 // code to make the site scroll to a specific place right away
 //window.scroll({
-  //top: 200,
+//top: 200,
 //  left: 0,
 //  behavior: 'smooth'});
